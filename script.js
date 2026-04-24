8c2e5b85051740b5aba7836d1b3e45c85205afca
egiindra <egiindraraditya377@gmail.com>
2026-04-24 22:46:26 +0700

    feat: create project SebayuFM

--- script.js
+++ script.js
@@ -17,6 +17,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+
     const sections = document.querySelectorAll("section");
     const scrollPos = window.scrollY + 100;
 
